7031169a4e619f14254fa9431bead7fede5fd82f
Andrey Gubarev <mylokin@me.com>
2016-09-06 13:08:38 +0300

    bump 1.3.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="1.3.2.4",
+      version="1.3.2.5",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson", "httplib2==0.9.2","six"],
